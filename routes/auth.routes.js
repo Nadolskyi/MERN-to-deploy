a8e7569bdd7704aac47783eacd79a9d74b113730
Nadolskyi <nadolskyy@i.ua>
2020-06-02 15:43:45 +0300

    added client

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/user');
 const router = Router();
@@ -42,7 +44,10 @@ router.post('/register',
   })
 
 router.post('/login',
-  [],
+  [
+    check('email', 'Bad email login').normalizeEmail().isEmail(),
+    check('password', 'pleaseEnterPassword').exists()
+  ],
   async (req, res) => {
     try {
 
@@ -51,24 +56,31 @@ router.post('/login',
       if (!errors.isEmpty()) {
         return res.status(400).json({
           errors: errors.array(),
-          message: 'wrong registration data'
+          message: 'wrong login data'
         })
       }
 
       const { email, password } = req.body;
 
-      const candidate = await User.findOne({ email })
+      const user = await User.findOne({ email })
 
-      if (candidate) {
-        return res.status(400).json({ message: 'Already is a user' })
+      if (!user) {
+        return res.status(400).json({ message: 'Cant find user' })
       }
 
-      const hashedPassword = await bcrypt.hash(password, 12);
-      const user = new User({ email, password: hashedPassword });
+      const isMatchPassword = await bcrypt.compare(password, user.password);
 
-      await user.save();
+      if (!isMatchPassword) {
+        return res.status(400).json({ message: 'Wrong password' })
+      }
 
-      res.status(201).json({ message: 'user created' });
+      const token = jwt.sign(
+        { userId: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: '1h' }
+      )
+
+      res.json({token, userId: user.id})
 
     } catch (e) {
       res.status(500).json({ message: 'Something wrong' })
